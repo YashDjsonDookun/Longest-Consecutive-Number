d68db2832d7e5bbbc9ece061820553cddf372c80
Yash Djson Dookun <dookun88@gmail.com>
2020-04-12 14:22:48 +0400

    Update consecutive.py

--- Longest_Consecutive_Number/consecutive.py
+++ Longest_Consecutive_Number/consecutive.py
@@ -1,27 +1,37 @@
 '''
 	This problem was recently asked by Amazon:
-	
 	You are given an array of integers. Return the length of the longest consecutive elements sequence in the array.
+
 	For example, the input array [100, 4, 200, 1, 3, 2] has the longest consecutive sequence 1, 2, 3, 4, and thus, you should return its length, 4.
 
 	def longest_consecutive(nums):
 	  # Fill this in.
+
 	print longest_consecutive([100, 4, 200, 1, 3, 2])
-	# 4
+	# should output: 4 <--
 '''
-
-
 class Consecutive:
-	def longest_consecutive(self, nums1):
-		consecutiveNums = 1 # My assumption is that a l unique number is a longest consecutive sequence in itself
+	def longest_consecutive(self, nums):
+		consecutiveNums = 1
+		largest = 1 # My assumption is that a unique number is a longest consecutive sequence in itself
 		# BubbleSort the array in ASC order
-		for i in range (len(nums1)):
-			for j in range (len(nums1) -1):
-				if (nums1[j] > nums1[j+1]):
-					nums1[j], nums1[j+1] = nums1[j+1], nums1[j]
+		for i in range (len(nums)):
+			for j in range (len(nums) -1):
+				if (nums[j] > nums[j+1]):
+					nums[j], nums[j+1] = nums[j+1], nums[j]
 		# Checks if next number is indeed an exact increment of one
-		for i in range (len(nums1) -1):
-			if (nums1[i]+1 == nums1[i+1]):
-				consecutiveNums = consecutiveNums + 1
-		return consecutiveNums
-#print(Consecutive().longest_consecutive([100,4,200,1,3,2]))
+		for i in range (len(nums) -1):
+			if (nums[i]+1 == nums[i+1]):
+				consecutiveNums += 1
+				if consecutiveNums > largest:
+					largest = consecutiveNums
+			else:
+				consecutiveNums = 1
+		print()
+		print(nums)
+		return largest
+
+# print(Consecutive().longest_consecutive([100,4,200,1,3,2]))
+# print(Consecutive().longest_consecutive([100,4,200,1,30,2]))
+# print(Consecutive().longest_consecutive([9,1,4,7,3,-1,0,5,8,-1,6]))
+# print(Consecutive().longest_consecutive([9,1,4,7,3,100,0,5,8,12,6]))
